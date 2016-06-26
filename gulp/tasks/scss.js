699d52b98d35d154f7c2c065e66249c67c7dc5c0
KUCKLU <kuck1u@visualive.jp>
2016-06-26 10:47:21 +0900

    no message

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,18 +4,14 @@ var conf = require("../config.js");
 var gulp = require("gulp");
 var $ = require("gulp-load-plugins")();
 var browserSync = require("browser-sync");
-var bourbon = require("node-bourbon");
-
-bourbon.with(conf.src + "/scss/style.scss");
 
 gulp.task("scss", function () {
     return gulp.src(conf.scss.src)
         .pipe($.sourcemaps.init())
-        .pipe($.sass({
-            includePaths: bourbon.includePaths
-        }).on("error", $.sass.logError))
+        .pipe($.sass().on("error", $.sass.logError))
         .pipe($.if("*.css", $.autoprefixer({"browsers": "last 2 versions"})))
         .pipe($.if("*.css", $.mergeMediaQueries()))
+        //.pipe($.if("*.css", $.csscomb()))
         .pipe(gulp.dest(conf.scss.dest))
         .pipe($.if("*.css", $.rename({suffix: ".min"})))
         .pipe($.if("*.css", $.csso()))
